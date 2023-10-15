bcb426d52575493ce7936bf6516cc711123854d8
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-10-15 10:59:44 -0300

    Inclusão de update único de dados

--- src/main/java/com/aylmerbolzan/todolist/task/TaskController.java
+++ src/main/java/com/aylmerbolzan/todolist/task/TaskController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aylmerbolzan.todolist.utils.Utils;
+
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.experimental.var;
 
@@ -57,9 +59,11 @@ private ITaskRepository taskRepository;
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 
 }
