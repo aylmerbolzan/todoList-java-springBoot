a424fa04c3268e2d3a9a4593a04f6fcc644e8a17
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-10-14 22:43:04 -0300

    Teste de validação de filtro

--- src/main/java/com/aylmerbolzan/todolist/user/UserController.java
+++ src/main/java/com/aylmerbolzan/todolist/user/UserController.java
@@ -8,15 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *  - Modificadores
- * public
- * private
- * protected
- * 
- */
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
- @RestController
+
+@RestController
  @RequestMapping("/users")
 public class UserController {
     
@@ -30,6 +25,10 @@ public class UserController {
       if(user != null) {
          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
       }
+
+      var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+      userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
