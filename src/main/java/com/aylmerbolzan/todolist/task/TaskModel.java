14ce70bc494238738527063032818cf81d16f29e
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-10-15 11:28:02 -0300

    Personalização de  error message - title > 50 char

--- src/main/java/com/aylmerbolzan/todolist/task/TaskModel.java
+++ src/main/java/com/aylmerbolzan/todolist/task/TaskModel.java
@@ -31,11 +31,11 @@ public class TaskModel {
      
      private UUID idUser;
 
-    //  public void setTitle(String title) throws Exception {
-    //     if (title.length() > 50) {
-    //       throw new Exception("O título deve ter no máximo 50 caracteres");
-    //     }
-    //     this.title = title;
-    //   }
+     public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+          throw new Exception("O título deve ter no máximo 50 caracteres");
+        }
+        this.title = title;
+      }
 
     }
